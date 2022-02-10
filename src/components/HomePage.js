b7349eddb0b3043bbafe72c6b65c3f7df86eddd7
Hooseinsalari <hossein.salari002@gmail.com>
2022-02-10 15:43:39 +0330

    add landing

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,10 +3,21 @@ import React from 'react';
 // styles
 import styles from "./HomePage.module.css";
 
+// img
+import logo from "../image/three.jpg"
+
 const HomePage = () => {
     return (
-        <div>
-            home page
+        <div className={styles.container}>
+            <header className={styles.header}>
+                <div className={styles.heafLeft}>
+                    <h1 className={styles.firstText}>WELCOME TO SHOP</h1>
+                    <h3 className={styles.text}>see store</h3>
+                </div>
+                <div className={styles.headRight}>
+                    <img src={logo} alt="logo" />
+                </div>
+            </header>
         </div>
     );
 };
